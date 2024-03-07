12457657e940482c638f1f0eae452e326eb1befb
Ahmed99x <152944430+Ahmed99x@users.noreply.github.com>
2024-03-07 23:27:57 +0200

    Delete Week_7/Mini Project directory

--- Week_7/Mini Project/DataTypes.h
+++
@@ -1,9 +0,0 @@
-#define MAX_NAMES 5
-#define MAX_NAME_LENGTH 20
-#define MAX_ITEMS 9
-
-typedef unsigned int u32;
-typedef unsigned int u8 ;
-typedef unsigned int u16;
-typedef   signed int s32;
-typedef        float f32;
